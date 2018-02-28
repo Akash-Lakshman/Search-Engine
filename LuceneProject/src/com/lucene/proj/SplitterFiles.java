ac77cd97cb584904c9c69e782a3bf1d257505aba
Akash-Lakshman <lakshmaa@tcd.ie>
2018-02-28 03:40:39 +0000

    First Commit to Split the files into individual documents for indexing

---
+++ LuceneProject/src/com/lucene/proj/SplitterFiles.java
@@ -0,0 +1,99 @@
+package com.lucene.proj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+
+public class SplitterFiles {
+
+	private static class Splitter extends Thread {
+		String inputFile;
+		String outputDirectory;
+
+		Splitter(String inputFile, String outputDirectory) {
+			super();
+			this.inputFile = inputFile;
+			this.outputDirectory = outputDirectory;
+		}
+
+		public void run() {
+			processFile(inputFile, outputDirectory);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		String inputDirectory = "Z:/SEM 2/Info Retrieval/lucene-7.2.1/CranFiles/";
+		Splitter documents_splitter = new Splitter(inputDirectory + "cran.all.1400", "Z:/SEM 2/Info Retrieval/lucene-7.2.1/ProductData/");
+		documents_splitter.start();
+		
+		try {
+				documents_splitter.join();
+			} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void processFile(String inputFile, String outputDirectory) {
+		BufferedReader bufferedReader = null;
+		String line;
+		String output = "";
+		Integer counter = 0;
+
+		try {
+			bufferedReader = new BufferedReader(new FileReader(new File(inputFile)));
+			bufferedReader.readLine();
+
+			while ((line = bufferedReader.readLine()) != null) {
+
+				System.out.println(line);
+				
+				if (line.startsWith(".T")) {
+					counter++;
+				}
+				else if (line.startsWith(".I")) {
+					writeOutput(outputDirectory + counter.toString() + ".txt", output);
+					output = "";
+				}
+				output = output + line + System.getProperty("line.separator");
+				 
+			}
+			writeOutput(outputDirectory + counter.toString() + ".txt", output);
+			bufferedReader.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (bufferedReader != null)
+				try {
+					bufferedReader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+
+	public static void writeOutput(String outputFileName, String output) throws IOException {
+		final File outputDirectory = new File(outputFileName);
+
+		if (!outputDirectory.exists()) {
+			outputDirectory.createNewFile();
+		}
+
+		Writer fileWriter = new FileWriter(outputDirectory);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		bufferedWriter.write(output);
+		//bufferedWriter.write(analyze(output));
+		bufferedWriter.close();
+		fileWriter.close();
+	}
+
+}
